6be1eecfc119c219033c1473a90b77db50f33d0d
Axel Torres <104744161+AxlAleT@users.noreply.github.com>
2025-03-09 10:56:25 -0600

    init

---
+++ src/main/java/com/escom/cerraduras/CerradurasApplication.java
@@ -0,0 +1,13 @@
+package com.escom.cerraduras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CerradurasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CerradurasApplication.class, args);
+    }
+
+}
